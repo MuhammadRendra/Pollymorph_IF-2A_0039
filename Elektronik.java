ed893bde4bdaa9d1746462f3e9979873435daaa8
MuhammadRendra <abas.bashori1612@gmail.com>
2026-05-13 09:49:37 +0700

    add inheritance

--- Elektronik.java
+++ Elektronik.java
@@ -1,3 +1,16 @@
-public class Elektronik {
+public class Elektronik extends Produk {
+
+    private int masaGaransi;
+
+    public Elektronik(String nama, double harga, int stok) {
+        super(nama, harga, stok);
+       this.masaGaransi = masaGaransi;
+    }
+
+    @Override
+    public double hitungHargaAkhir(int jmlhBeli) {
+      
+     
+    }
     
 }
