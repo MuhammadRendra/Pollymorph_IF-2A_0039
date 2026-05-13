cea37ad2f55e603a59c3c9d904fb35fda0f1e8dd
MuhammadRendra <abas.bashori1612@gmail.com>
2026-05-13 10:10:56 +0700

    add upcasting

--- Elektronik.java
+++ Elektronik.java
@@ -2,7 +2,7 @@ public class Elektronik extends Produk {
 
     private int masaGaransi;
 
-    public Elektronik(String nama, double harga, int stok) {
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
         super(nama, harga, stok);
         this.masaGaransi = masaGaransi;
     }
--- Makanan.java
+++ Makanan.java
@@ -1,7 +1,7 @@
 public class Makanan extends Produk {
     private int kadaluarsa;
 
-    public Makanan(String nama, double harga, int stok) {
+    public Makanan(String nama, double harga, int stok, int kadaluarsa) {
         super(nama, harga, stok);
         this.kadaluarsa = kadaluarsa;
     }
