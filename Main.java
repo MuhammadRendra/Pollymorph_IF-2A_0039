26a912214f8a63ba7d73810a5818e457f7fa90d6
MuhammadRendra <abas.bashori1612@gmail.com>
2026-05-13 10:13:34 +0700

    add polymorphism Makanan

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Produk p1 = new Elektronik("Samsung", 10000, 5, 2);
         Produk p2 = new Makanan("Bakso", 1000, 100, 3);
         Kasir kasir = new Kasir("MAMAN");
-        
+
         p1.tampilkanInfo();
         kasir.tampilkanIdentitas();
         kasir.prosesBelanja(p1, 3);
+
+        System.out.println();
+        System.out.println();
+        p2.tampilkanInfo();
+        kasir.tampilkanIdentitas();
+        kasir.prosesBelanja(p2, 11);
     }
 }
