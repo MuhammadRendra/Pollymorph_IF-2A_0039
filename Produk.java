3cc36aaebc4ca23b7b2f7ba1f4ded0b2d7a901db
MuhammadRendra <abas.bashori1612@gmail.com>
2026-05-13 09:45:21 +0700

    add method tampilkan info

--- Produk.java
+++ Produk.java
@@ -1,17 +1,26 @@
-public abstract class Produk{
+public abstract class Produk {
     private String nama;
     private double harga;
     private int stok;
 
-    public Produk(String nama, double harga, int stok){
+    public Produk(String nama, double harga, int stok) {
         this.nama = nama;
         this.harga = harga;
         this.stok = stok;
     }
-    public void setStok(int jmlhBeli){
-        stok = stok-jmlhBeli;
+
+    public void setStok(int jmlhBeli) {
+        stok = stok - jmlhBeli;
     }
-    public int getStok(){
+
+    public int getStok() {
         return stok;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("====Produk====");
+        System.out.println("Nama: " + nama);
+        System.out.println("Harga: " + harga);
+        System.out.println("Stok: " + stok);
     }
 }
